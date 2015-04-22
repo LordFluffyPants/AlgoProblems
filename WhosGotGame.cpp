3f8fb7ff18f50c8f5de1d16d5ebe44fcd6abc15e
LordFluffyPants <rixmaddi@gmail.com>
2015-04-22 19:21:19 -0400

    DFS implementation of cyclic check

--- WhosGotGame.cpp
+++ WhosGotGame.cpp
@@ -12,14 +12,66 @@
 #include <vector>
 #include <list>
 
-void printVector(std::vector<int> *pMatrix, int V){
-    for (int k = 1; k < V; k++){
-    
-        for(std::vector<int>::const_iterator i = pMatrix[k].begin(); i != pMatrix[k].end(); ++i){
-        std::cout << *i << std::endl;
+/**
+ * Recursively calls the function to check if the node has been not visited.
+ * If it has not been visited then sets visited in the bool stack to be true and the recStack to be true as well to check later in recursive calls.
+ * then loops through each node to check for the next node using DFS. If the node loops onto itself i.e. you need 4 to unlock 4 then it would be cyclic. If the child node is not visited and the child node using the recursive call is able to make it back to the child then it will be cyclic.
+ */
+bool isCyclicRecursive(std::vector<int> *pMatrix, int V, bool visited[], bool *recStack){
+    if (visited[V] == false){
+        visited[V] = true;
+        recstack[V] = true;
+                
+        for(std::vector<int>::const_iterator i = pMatrix[V].begin(); i != pMatrix[V].end(); ++i){
+            if (*i == V) return true;
+            if (!visited[*i] && isCyclicRecursive(*i, visited, recStack))
+                return true;
+            else if (recStack[*i])
+                return true; 
         }
     }
+    recStack [V] = false;
+    return false;
+}
+
+/**
+ * This method will check if the adjMatrix is cyclic or not
+ * It inits the visited and recStack matrixes to false as they have not visited any node
+ */
+bool isCyclic(std::vector<int> *pMatrix, int V){  
     
+    bool *visited = new bool[V];
+    bool *recStack = new bool[V];
+    for (int i = 1; i < V; i ++){
+        visited[i] = false;
+        recStack[i] = false;
+    }   
+    for (int i = 1; i < V; i++)
+        if (isCyclicRecursive(pMatrix,i, visited, recStack))
+            return true;
+    return false;   
+}
+
+/**
+ * This method will check id the adjMatrix is linear or not.
+ * If the node has more than one child then it would be non linear gameplay thus would not be linear.
+ */
+bool isLinear(std::vector<int> *pMatrix, int V){
+    //TODO Implement Method
+    
+    //This loop checks if the matrix has multiple children connecting it 
+    //if the loop has multiple children then the gameplay is non linear    
+    for (int k = 1; k < V; k++)
+        if (pMatrix[k].size() > 1) 
+            return false;
+    
+    bool *visited = new bool[V];
+    bool *recStack = new bool[V];
+    for (int i = 1; i < V; i ++){
+        visited[i] = false;
+        recStack[i] = false;
+    }
+          
 }
 
 /**
@@ -50,7 +102,20 @@ int main(int argc,char*  argv[]){
             adjMatrix[d].push_back(u);        
                        
         }
-        printVector(adjMatrix, n+1);
+        //printVector(adjMatrix, n+1);
+
+        //This checks for what type of gameplay is possible
+        if (isCyclic(adjMatrix, n+1)){ 
+            std::cout << "Infeasable Game" << std::endl;
+        }
+        else if (isLinear(adjMatrix, n+1)){
+            std::cout<< "Linear Gameplay" << std::endl;
+        }
+        else {
+            std::cout << "Non-Linear Gameplay Possible" << std::endl;
+        }
+        
+        //Incriments I to go to the next inputed value
         i += (atoi(argv[i+1])*2) + 2;
     }
     std::cout << "out of loop" << std::endl;
